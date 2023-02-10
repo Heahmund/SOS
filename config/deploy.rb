2eb1e706db517e497a1f16efd94ee74edf4ead32
Heahmund <tima260399@mail.ru>
2023-02-10 15:44:48 +0300

    123

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :format, :pretty
 
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/private_pub.yml"
+append :linked_files, "config/database.yml", "config/private_pub.yml", ".env"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "bin", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system"
