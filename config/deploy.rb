e2f490934b94da305e0bd52996ec8e22f5f556d8
Heahmund <tima260399@mail.ru>
2023-02-10 15:04:25 +0300

    qwd

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.2.1"
+lock "~> 3.17.1"
 
 set :application, "sos"
 set :repo_url, "git@github.com:Heahmund/SOS.git"
@@ -13,28 +13,15 @@ set :deploy_user, "musaev"
 # Default value for :format is :airbrussh.
 set :format, :pretty
 
-set :local_user
+
 
 # Default value for :linked_files is []
-append :linked_files, %w{config/database.yml 'config/master.key' 'config/private_pub.yml'}
+append :linked_files, %w{config/database.yml 'config/private_pub.yml'}
 
 # Default value for linked_dirs is []
-append :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-# append :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+append :linked_dirs, "bin", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system"
+
 # Default value for local_user is ENV['USER']
-set :local_user, "musaev"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-
-namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :publishing, :restart
-end
