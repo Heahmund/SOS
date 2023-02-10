5edc8997b2e82a121f1e2325df5cfe8d70e28dd3
Heahmund <tima260399@mail.ru>
2023-02-10 15:19:54 +0300

    help

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.1"
 
-set :application, "sos"
+set :application, "SOS"
 set :repo_url, "git@github.com:Heahmund/SOS.git"
 
 # Default branch is :Second
-ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/musaev/deployer/sos"
+set :deploy_to, "/home/musaev/deployer/SOS"
 set :deploy_user, 'musaev'
 # Default value for :format is :airbrussh.
 set :format, :pretty
@@ -16,10 +16,10 @@ set :format, :pretty
 
 
 # Default value for :linked_files is []
-append :linked_files, %w{config/database.yml config/private_pub.yml}
+append :linked_files, "config/database.yml", "config/private_pub.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+append :linked_dirs, "bin", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system"
 
 
 # Default value for local_user is ENV['USER']
