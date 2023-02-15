836b794bab23391ba6bed3722beaea0903e95b19
Heahmund <tima260399@mail.ru>
2023-02-15 08:48:08 +0300

    eager_load_paths

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,8 @@ module SOS
       g.fixture_replacement :factory_bot, dir: 'spec/factories'
 
       # config.autoload_paths << Rails.root.join('lib/middleware')
-      config.eager_load_paths << "#{Rails.root}/lib"
+      # config.eager_load_paths << "#{Rails.root}/lib"
+      config.eager_load_paths << Rails.root.join('lib')
       # config.middleware.insert_after Rack::Runtime, 'DailyRateLimit' unless Rails.env.test?
     end
   end
