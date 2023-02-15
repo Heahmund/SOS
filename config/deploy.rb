37b4671a470ff4d07911174ede95fddf99bcf556
Heahmund <tima260399@mail.ru>
2023-02-15 11:00:03 +0300

    wd

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,7 +58,7 @@ task :stop do
   on roles(:app) do
    within current_path do
     with rails_env: fetch(:rails_env)do
-      execute :bundle, "exec thin -C config/private_pub_thin.yml start"
+      execute :bundle, "exec thin -C config/private_pub_thin.yml stop"
     end
   end
 end
@@ -74,3 +74,5 @@ task :restart do
   end
 end
 end
+
+after 'deploy:restart', 'private_pub:restart'
